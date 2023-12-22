e8c78847fae9aa6594d79f7d4e81855edc252921
thfruchart <66477650+thfruchart@users.noreply.github.com>
2023-12-22 17:23:33 +0100

    Add files via upload

--- proj/Pyxel/Jeu.py
+++ proj/Pyxel/Jeu.py
@@ -1,7 +1,7 @@
 import pyxel
 from random import randint
 
-DELAI = 10
+
 LARG = 128
 HAUT = 128
 CASE = 8
@@ -9,6 +9,7 @@ VECT_DIR = {'Haut':(0,-1), 'Bas':(0,1), 'Gauche':(-1,0), 'Droite':(1,0)}
 
 pyxel.init(LARG, HAUT)
 pyxel.load("monfichier.pyxres")
+delai = 10
 pomme = [0,0]
 serpent = [(10,1),(11,1),(12,1)]
 direction = 'Gauche'
@@ -22,25 +23,41 @@ def new_pomme():
         x = randint(0, LARG//CASE-1)
         y = randint(0, HAUT//CASE-1)
     pomme[0] = x
-    pomme[1] = x
+    pomme[1] = y
 
 new_pomme()
+
+
+def reinitialiser():
+    global serpent, direction, gameover, score, delai
+    serpent = [(10,1),(11,1),(12,1)]
+    direction = 'Gauche'
+    gameover = False
+    score = 0
+    delai = 10
+    
 
 def update():
     global direction
     global gameover
+    global delai
     global score
     if gameover :
-        return
-    if pyxel.btn(pyxel.KEY_DOWN) :
+        if pyxel.btn(pyxel.KEY_N) :
+            pyxel.quit()
+        elif pyxel.btn(pyxel.KEY_O):
+            reinitialiser()
+        else :
+            return
+    if pyxel.btn(pyxel.KEY_DOWN) and direction != 'Haut' :
         direction = 'Bas'
-    if pyxel.btn(pyxel.KEY_UP) :
+    if pyxel.btn(pyxel.KEY_UP) and direction != 'Bas':
         direction = 'Haut'
-    if pyxel.btn(pyxel.KEY_LEFT) :
+    if pyxel.btn(pyxel.KEY_LEFT) and direction != 'Droite':
         direction = 'Gauche'
-    if pyxel.btn(pyxel.KEY_RIGHT) :
+    if pyxel.btn(pyxel.KEY_RIGHT) and direction != 'Gauche':
         direction = 'Droite'
-    if pyxel.frame_count % DELAI == 0 :
+    if pyxel.frame_count % delai == 0 :
         #coordonnée du premier élément du serpent
         x,y = serpent[0]
         #coordonnées du déplacement
@@ -53,11 +70,15 @@ def update():
         if tete == (pomme[0], pomme[1]):
             score += 1
             new_pomme()
+            if delai > 2:
+                delai -=1
         else : 
             serpent.pop()
         if tete[0]<0 or tete[0] >= LARG//CASE :
             gameover = True
         if tete[1]<0 or tete[1] >= HAUT//CASE :
+            gameover = True
+        if tete in serpent[1:]:
             gameover = True
         
             
@@ -66,6 +87,7 @@ def update():
 def draw():
     if gameover :
         pyxel.text(10,10,"GAME OVER",6)
+        pyxel.text(LARG // 3, HAUT //2 ,"REJOUER (O/N)?",6)
         return
     pyxel.cls(0)
     pyxel.text(0,0,"Score : "+ str(score),6)
